b2876d5dcdd71ee65203a4a60cd3aca2d1e5459b
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-12-24 18:03:36 +0530

    Create workExperience.js

---
+++ backend_sanity/schemas/workExperience.js
@@ -0,0 +1,27 @@
+export default {
+    name:'workExperience',
+    title:'Work Experience',
+    type:'document',
+    fields:[
+           {
+                name:'name',
+                title:'Name',
+                type:'string'
+            },
+            {
+                name:'company',
+                title:'Company',
+                type:'string'
+            },
+            {
+                name:'desc',
+                title:'Desc',
+                type:'string'
+            },
+            {
+                name:'duration',
+                title:'Duration',
+                type:'string'
+            }
+    ]
+}
