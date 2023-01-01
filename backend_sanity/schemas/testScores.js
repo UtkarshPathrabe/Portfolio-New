1131d3f4fefc4c0c7a390ba69c76aa8b6c28aa82
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-01-01 13:31:33 +0530

    Create testScores.js

---
+++ backend_sanity/schemas/testScores.js
@@ -0,0 +1,27 @@
+export default {
+  name: 'testScore',
+  title: 'TestScore',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'score',
+      title: 'Score',
+      type: 'string',
+    },
+    {
+      name: 'date',
+      title: 'Date',
+      type: 'string',
+    },
+    {
+      name: 'rank',
+      title: 'Rank',
+      type: 'number',
+    }
+  ],
+};
