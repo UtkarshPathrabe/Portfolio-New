ddf093c5741bde4a86d44978949ee17fae2da7d6
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-01-05 21:53:46 +0530

    Update About.jsx

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -19,7 +19,7 @@ const About = () => {
 
   return (
     <>
-      <h2 className='head-text'>I Know That <span>Good Apps</span> <br /> means <span>Good Business</span></h2>
+      <h2 className='head-text'>It is never too late to <span>learn something</span></h2>
       <div className='app__profiles'>
         { abouts.map((about, index) => (
           <motion.div
@@ -34,6 +34,16 @@ const About = () => {
             <p className='p-text' style={{ marginTop: 10 }}>{about.description}</p>
           </motion.div>
         )) }
+        <motion.div
+          whileInView={{ opacity: 1 }}
+          whileHover={{ scale: 1.1 }}
+          transition={{ duration: 0.5, type: 'tween' }}
+          className='app__profile-about'
+        >
+          <p className='bold-text'>I am a Senior System Software Engineer at NVIDIA Graphics Pvt. Ltd, Pune, India.</p>
+          <p className='bold-text'>I am Computer Science Graduate from BITS Pilani, Rajasthan, India.</p>
+          <p className='bold-text'>I like solving competitive coding problems, exploring new technologies, drawing, painting, travelling and gaming.</p>
+        </motion.div>
       </div>
     </>
   );
