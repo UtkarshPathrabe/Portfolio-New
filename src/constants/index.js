f6d9565b3c1c15c15b2fb84530cf77063809ef1c
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-01-07 15:35:31 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,7 @@
 export { default as images } from './images';
+export { getDateDiffInDays, fetcher, reactSvgComponentToMarkupString } from './utils';
+export const SERVER_BASE_URL = process.env.REACT_APP_SERVER_BASE_URL;
+
 export const HOME_LINK = 'home';
 export const ABOUT_LINK = 'about';
 export const WORK_LINK = 'projects';
@@ -9,5 +12,6 @@ export const PAINTINGS_LINK = 'paintings';
 export const CONTACT_LINK = 'contact';
 export const navLinks = [ABOUT_LINK, WORK_LINK, SKILLS_LINK, CERTIFICATES_LINK, ACCOMPLISHMENTS_LINK, PAINTINGS_LINK, CONTACT_LINK];
 export const navDotsLinks = [HOME_LINK, ABOUT_LINK, WORK_LINK, SKILLS_LINK, CERTIFICATES_LINK, ACCOMPLISHMENTS_LINK, PAINTINGS_LINK, CONTACT_LINK];
+
 export const ALL_TAG = 'All';
 export const PROJECT_TAGS = [ALL_TAG, 'AI', 'Full-Stack', 'JavaScript', 'Python', 'PWA', 'R', 'React', 'SpringBoot', 'ThreeJS', 'Web 3.0'];
