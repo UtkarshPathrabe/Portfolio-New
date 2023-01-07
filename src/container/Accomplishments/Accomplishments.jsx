450b961f45817c16136469ecfa8ad28e92389414
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-01-07 15:35:35 +0530

    Update Accomplishments.jsx

--- src/container/Accomplishments/Accomplishments.jsx
+++ src/container/Accomplishments/Accomplishments.jsx
@@ -1,16 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import { motion } from 'framer-motion';
+import React, { useState } from 'react';
 
 import { AppWrap, MotionWrap } from '../../wrapper';
 
 import './Accomplishments.scss';
 import { ACCOMPLISHMENTS_LINK } from '../../constants';
+import LeetCode from './LeetCode';
 
 const Accomplishments = () => {
 
   return (
     <>
-      <h2 className='head-text'>My <span>Accomplishments</span></h2>
+      <h2 className='head-text' style={{ marginBottom: '2rem' }}>My <span>Accomplishments</span></h2>
+      <LeetCode />
     </>
   );
 };
