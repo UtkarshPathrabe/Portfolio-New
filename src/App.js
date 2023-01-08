5ddda11d92a3fc715cfc1727d22d40007b95ab02
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-01-08 15:42:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import { About, Footer, Header, Skills, Certificates, Work, Paintings, Accomplishments } from './container';
-import { Navbar } from './components';
+import { Navbar, NavigationDots, SocialMedia } from './components';
 import './App.scss';
+import { OnScreenContext } from './context';
 
 const App = () => {
+  const { inView } = useContext(OnScreenContext);
+
   return (
     <div className='app'>
       <Navbar />
@@ -16,6 +19,8 @@ const App = () => {
       <Accomplishments />
       <Paintings />
       <Footer />
+      <SocialMedia />
+      <NavigationDots active={inView} />
     </div>
   );
 };
