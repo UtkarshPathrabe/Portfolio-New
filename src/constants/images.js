f55fb7633c352ed76e6abc28d095b7c77258c113
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-01-08 21:02:35 +0530

    Update images.js

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,22 +1,8 @@
 import email from '../assets/email.png';
 import mobile from '../assets/mobile.png';
-import api from '../assets/api.png';
-import cpp from '../assets/cpp.png';
-import css from '../assets/css.png';
-import figma from '../assets/figma.png';
-import flutter from '../assets/flutter.png';
-import git from '../assets/git.png';
-import graphql from '../assets/graphql.png';
-import html from '../assets/html.png';
-import javascript from '../assets/javascript.png';
-import mu5 from '../assets/mu5.png';
-import node from '../assets/node.png';
 import python from '../assets/python.png';
 import react from '../assets/react.png';
-import redux from '../assets/redux.png';
-import sass from '../assets/sass.png';
 import typescript from '../assets/typescript.png';
-import vue from '../assets/vue.png';
 
 import about01 from '../assets/about01.png';
 import about02 from '../assets/about02.png';
@@ -31,23 +17,9 @@ import logo from '../assets/utkarsh-name-logo.png';
 const images = {
   email,
   mobile,
-  api,
-  cpp,
-  css,
-  figma,
-  flutter,
-  git,
-  graphql,
-  html,
-  javascript,
-  mu5,
-  node,
   python,
   react,
-  redux,
-  sass,
   typescript,
-  vue,
   about01,
   about02,
   about03,
