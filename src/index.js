7b2d741c5d8589f670696d13f8929b8ccc946266
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-01-08 15:47:55 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+import { OnScreenProvider } from './context';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <OnScreenProvider>
+    <App />
+  </OnScreenProvider>,
+document.getElementById('root'));
