bca17f7a02fce07156c24657c7a439e4851327df
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-01-08 15:47:59 +0530

    Update AppWrap.js

--- src/wrapper/AppWrap.js
+++ src/wrapper/AppWrap.js
@@ -1,20 +1,28 @@
-import React from 'react';
-import { NavigationDots, SocialMedia } from '../components';
+import React, { useContext } from 'react';
+import { useInView } from 'react-intersection-observer';
+import { useEffect } from 'react';
+import { OnScreenContext } from '../context';
 
 const AppWrap = (Component, idName, classNames) => function HOC() {
-  return (
-    <div id={idName} className={`app__container ${classNames}`}>
-      <SocialMedia />
-      <div className='app__wrapper app__flex'>
-        <Component />
-        <div className='copyright'>
-          <p className='p-text'>ⓒ Utkarsh Pathrabe</p>
-          <p className='p-text'>All rights reserved</p>
-        </div>
+
+  const { ref, inView } = useInView({ threshold: 0.1, });
+  const { setInView } = useContext(OnScreenContext);
+
+  useEffect(() => {
+    if (inView) {
+      setInView(idName);
+    }
+  }, [inView, setInView]);
+
+  return (<section id={idName} className={`app__container ${classNames}`}>
+    <div className='app__wrapper app__flex' ref={ref}>
+      <Component />
+      <div className='copyright'>
+        <p className='p-text'>ⓒ Utkarsh Pathrabe</p>
+        <p className='p-text'>All rights reserved</p>
       </div>
-      <NavigationDots active={idName} />
     </div>
-  );
+  </section>);
 };
 
 export default AppWrap;
