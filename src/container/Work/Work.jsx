8a2c2e1ca34cf9c5ed359894007cf44808098dd8
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-04-17 22:40:57 +0530

    Update Work.jsx

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -66,7 +66,7 @@ const Work = () => {
                 transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
                 className="app__work-hover app__flex"
               >
-                <a href={work.projectLink} target="_blank" rel="noreferrer">
+                {(work.projectLink) ? <a href={work.projectLink} target="_blank" rel="noreferrer">
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
                     whileHover={{ scale: [1, 0.9] }}
@@ -75,7 +75,7 @@ const Work = () => {
                   >
                     <AiFillEye />
                   </motion.div>
-                </a>
+                </a> : null}
                 <a href={work.codeLink} target="_blank" rel="noreferrer">
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
