8096da9461938f1a4ec518afc4bc0e1ec3259d06
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-06-14 22:36:38 +0530

    Update Work.jsx

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -60,7 +60,7 @@ const Work = () => {
         {filterWork.map((work, index) => (
           <div className='app__work-item app__flex' key={index}>
             <div className='app__work-img app__flex'>
-              <img src={urlFor(work.imgUrl)} alt={work.name} />
+              <img src={urlFor(work.imgUrl)} alt={work.name} loading='lazy' />
               <motion.div
                 whileHover={{ opacity: [0, 1] }}
                 transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
