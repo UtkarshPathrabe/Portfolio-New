719406b65ca46d3597cd52164fb4d7fba2e0a2bb
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-06-14 22:36:18 +0530

    Update GitHub.jsx

--- src/container/Accomplishments/GitHub.jsx
+++ src/container/Accomplishments/GitHub.jsx
@@ -10,6 +10,7 @@ const GitHub = () => {
         transition={{ duration: 0.25 }}
         src='https://raw.githubusercontent.com/UtkarshPathrabe/UtkarshPathrabe/main/profile-3d-contrib/profile-south-season-animate.svg'
         alt='github_stats_chart'
+        loading='lazy'
       />
     </div>
     <div className="app__accomplishments-github-details-stats-chart">
@@ -18,6 +19,7 @@ const GitHub = () => {
         transition={{ duration: 0.25 }}
         src='https://github-profile-trophy.vercel.app/?username=utkarshpathrabe&theme=transparent&column=6&margin-w=2&margin-h=2&no-bg=true&no-frame=true&rank=-C,-?'
         alt='github_stats'
+        loading='lazy'
       />
     </div>
     <div className="app__accomplishments-github-details-stats-chart_container">
@@ -26,12 +28,14 @@ const GitHub = () => {
         transition={{ duration: 0.25 }}
         src='https://github-readme-stats.vercel.app/api?username=UtkarshPathrabe&show_icons=true&theme=transparent&border_radius=10px&count_private=true'
         alt='github_stats'
+        loading='lazy'
       />
       <motion.img
         whileHover={{ scale: [1, 1.05] }}
         transition={{ duration: 0.25 }}
         src='https://github-readme-stats.vercel.app/api/top-langs/?username=UtkarshPathrabe&theme=transparent&border_radius=10px&layout=compact&langs_count=10'
         alt='github_stats'
+        loading='lazy'
       />
     </div>
   </div>);
