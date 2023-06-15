9bfbcf992d893b3dd043109f4393b729bc8c2dc0
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-06-15 20:43:30 +0530

    Update images.js

--- src/constants/images.js
+++ src/constants/images.js
@@ -4,11 +4,6 @@ import python from '../assets/python.png';
 import react from '../assets/react.png';
 import typescript from '../assets/typescript.png';
 
-import about01 from '../assets/about01.png';
-import about02 from '../assets/about02.png';
-import about03 from '../assets/about03.png';
-import about04 from '../assets/about04.png';
-
 import profile from '../assets/utkarsh-profile.webp';
 import utkarshImage from '../assets/profile01.webp';
 import circle from '../assets/circle.svg';
@@ -20,10 +15,6 @@ const images = {
   python,
   react,
   typescript,
-  about01,
-  about02,
-  about03,
-  about04,
   profile,
   circle,
   logo,
