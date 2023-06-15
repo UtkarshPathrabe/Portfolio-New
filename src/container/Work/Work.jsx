e8a5fcada1dd4748fdc05a8817d59f19e18ae049
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-06-15 09:03:30 +0530

    Update Work.jsx

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -60,13 +60,13 @@ const Work = () => {
         {filterWork.map((work, index) => (
           <div className='app__work-item app__flex' key={index}>
             <div className='app__work-img app__flex'>
-              <img src={urlFor(work.imgUrl)} alt={work.name} loading='lazy' />
+              <img src={urlFor(work.imgUrl)} alt={`work-${work.title}-${index}`} loading='lazy' />
               <motion.div
                 whileHover={{ opacity: [0, 1] }}
                 transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
                 className="app__work-hover app__flex"
               >
-                {(work.projectLink) ? <a href={work.projectLink} target="_blank" rel="noreferrer">
+                {(work.projectLink) ? <a href={work.projectLink} target="_blank" rel="noreferrer" title={`work-link-${work.title}-${index}`}>
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
                     whileHover={{ scale: [1, 0.9] }}
@@ -76,7 +76,7 @@ const Work = () => {
                     <AiFillEye />
                   </motion.div>
                 </a> : null}
-                <a href={work.codeLink} target="_blank" rel="noreferrer">
+                <a href={work.codeLink} target="_blank" rel="noreferrer" title={`work-source-code-${work.title}-${index}`}>
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
                     whileHover={{ scale: [1, 0.9] }}
@@ -89,7 +89,7 @@ const Work = () => {
               </motion.div>
             </div>
             <div className='app__work-content app__flex' data-tip data-for={work.title}>
-              <h4 className='bold-text'>{work.title}</h4>
+              <h3 className='bold-text'>{work.title}</h3>
               <div className='app__work-tag app__flex'>
                 <p className='p-text'>{work.tags.join(' || ')}</p>
               </div>
