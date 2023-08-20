396c29c1ac76ea2e791ff548ca766d545baca1cd
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-08-20 20:08:56 +0530

    Update About.jsx

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -23,7 +23,7 @@ const About = () => {
       <div className='app__profiles-container'>
         <motion.div
           whileInView={{ opacity: 1 }}
-          whileHover={{ scale: 1.04 }}
+          whileHover={{ scale: 1.03 }}
           transition={{ duration: 0.5, type: 'tween' }}
           className='app__profiles-image'
         >
@@ -33,7 +33,7 @@ const About = () => {
           { abouts.map((about, index) => (
             <motion.div
               whileInView={{ opacity: 1 }}
-              whileHover={{ scale: 1.1 }}
+              whileHover={{ scale: 1.03 }}
               transition={{ duration: 0.5, type: 'tween' }}
               className='app__profile-item'
               key={about.title + index}
@@ -43,16 +43,57 @@ const About = () => {
               <p className='p-text' style={{ marginTop: 10 }}>{about.description}</p>
             </motion.div>
           )) }
-          <motion.div
-            whileInView={{ opacity: 1 }}
-            whileHover={{ scale: 1.05 }}
-            transition={{ duration: 0.5, type: 'tween' }}
-            className='app__profile-about'
-          >
-            <p className='bold-text'>I am a Senior System Software Engineer at NVIDIA Graphics Pvt. Ltd, Pune, India.</p>
-            <p className='bold-text'>I am Computer Science Graduate from BITS Pilani, Rajasthan, India.</p>
-            <p className='bold-text'>I like solving competitive coding problems, exploring new technologies, drawing, painting, travelling and gaming.</p>
-          </motion.div>
+          <div className='app__profile-about'>
+            <motion.p
+              whileInView={{ opacity: 1 }}
+              whileHover={{ scale: 1.03 }}
+              transition={{ duration: 0.5, type: 'tween' }}
+            >
+              I currently hold the esteemed position of Senior System Software Engineer at NVIDIA Graphics Pvt. Ltd, based in Pune, India. My professional journey has been marked by significant roles at prominent organizations. I previously served as a Senior Associate Software Engineer at J P Morgan Chase & Co. in Bengaluru, where I contributed to impactful projects. I also had the privilege of interning at J P Morgan Chase & Co. in Mumbai, further expanding my experience within the organization.
+            </motion.p>
+            <motion.p
+              whileInView={{ opacity: 1 }}
+              whileHover={{ scale: 1.03 }}
+              transition={{ duration: 0.5, type: 'tween' }}
+            >
+              During my growth-oriented journey, I gained valuable insights as a Data Analyst Summer Intern at InfoCepts Technologies Pvt. Ltd. in Nagpur, where I honed my analytical skills. My journey also included a Summer Internship at CMC Ltd. in Mumbai, which provided me with hands-on exposure to the industry.
+            </motion.p>
+            <motion.p
+              whileInView={{ opacity: 1 }}
+              whileHover={{ scale: 1.03 }}
+              transition={{ duration: 0.5, type: 'tween' }}
+            >
+              I am a proud alumnus of BITS Pilani, Rajasthan, India, where I pursued a B.E. (Hons) in Computer Science. My academic journey culminated in an impressive 9.33 CGPA, underscoring my dedication to excellence in education.
+            </motion.p>
+            <motion.p
+              whileInView={{ opacity: 1 }}
+              whileHover={{ scale: 1.03 }}
+              transition={{ duration: 0.5, type: 'tween' }}
+            >
+              Beyond my professional and academic endeavors, I am an enthusiast of competitive coding. Tackling intricate problems not only stimulates my intellect but also fortifies my problem-solving abilities. My curiosity about emerging technologies is unceasing, motivating me to delve into new realms of innovation and knowledge.
+            </motion.p>
+            <motion.p
+              whileInView={{ opacity: 1 }}
+              whileHover={{ scale: 1.03 }}
+              transition={{ duration: 0.5, type: 'tween' }}
+            >
+              Artistry is a crucial part of my life, with a penchant for drawing and painting. These creative outlets offer a balance to my technical pursuits and serve as vehicles for self-expression. I am also an avid traveler, fueled by an adventurous spirit that seeks to explore diverse cultures and places, enriching my perspectives.
+            </motion.p>
+            <motion.p
+              whileInView={{ opacity: 1 }}
+              whileHover={{ scale: 1.03 }}
+              transition={{ duration: 0.5, type: 'tween' }}
+            >
+              Gaming is another facet of my interests, offering an immersive world that fuels my imagination and creativity.
+            </motion.p>
+            <motion.p
+              whileInView={{ opacity: 1 }}
+              whileHover={{ scale: 1.03 }}
+              transition={{ duration: 0.5, type: 'tween' }}
+            >
+              In a nutshell, my journey has been a tapestry of academic excellence, professional growth, and personal passions, all contributing to a well-rounded individual committed to continuous learning and enrichment.
+            </motion.p>
+          </div>
         </div>
       </div>
     </>
